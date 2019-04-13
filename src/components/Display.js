af33d78e7214ec46483eac6387ce2d39f410ea87
Jonathan Dorety <kyullog@gmail.com>
2019-04-13 13:38:31 -0400

    Display can pass props to be rendered

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,6 +4,16 @@ const Display = props => {
   return (
     <>
       <h2>Count</h2>
+      <div className="wrapper">
+        <>
+          <h3>Balls</h3>
+          <p>{props.balls}</p>
+        </>
+        <>
+          <h3>Strikes</h3>
+          <p>{props.strikes}</p>
+        </>
+      </div>
     </>
   );
 };
--- src/components/Display.test.js
+++ src/components/Display.test.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render, cleanup } from "react-testing-library";
 
 import Display from "./Display";
 
 describe("Display component tests", () => {
   it("renders w/out crashing", () => {
     render(<Display />);
+  });
+  it("should display a header to label counts", () => {
+    const { getByText } = render(<Display />);
+    getByText(/strikes/i, /balls/i);
+    cleanup();
+  });
+  it("should render two numbers to represent the count", () => {
+    const { getByText } = render(<Display strikes={2} balls={3} />);
+    getByText(/2/i, /3/i);
   });
 });
